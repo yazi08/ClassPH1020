b86d49e5930c9337e7960cd2afd2ad73528e500a
yazi08 <yaroslav24.01.1998@mail.ru>
2021-04-21 20:20:03 +0300

    Update my_input.py
    добавили печать на экран

--- my_input.py
+++ my_input.py
@@ -1,8 +1,17 @@
 class Ввод():
-	def __call__ (*args,**kwargs):
-		return input(*args,**kwargs)
+    def __call__ (self,*args,**kwargs):
+	    return input(*args,**kwargs)
+
+def печать(*args,**kwargs):
+    return print(*args,**kwargs)
+
+def main():
+    ввод_имени = Ввод()
+    ввод_возраста = Ввод()
+    имя = ввод_имени("ваше имя")
+    лет = ввод_возраста("ваш возраст")
+    печать(f"Привет {имя} {лет}")
 
 
+if __name__ == "__main__":main()
 
-ввод_имени = Ввод()
-ввод_возраста = Ввод()
